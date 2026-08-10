77700f273127141e5f3a2b4f49f7c6210c9c70af
amiralikhorasani <amiralik163@gmail.com>
2026-08-10 22:22:02 +0330

    refactor: update shared UI styles

--- src/components/ui/Container.jsx
+++ src/components/ui/Container.jsx
@@ -1,7 +1,7 @@
 function Container({ children, className = "", ...props }) {
   return (
     <div
-      className={`rounded-xl bg-linear-to-br from-gray-950 to-gray-900 p-5 text-slate-300 shadow-[inset_0_0_0_1px_theme(colors.slate.800)] backdrop-blur-xl ${className}`}
+      className={`rounded-xl border-[1.7px] border-t-gray-800 border-r-violet-200/30 border-b-violet-200/30 border-l-gray-800 bg-linear-to-br from-gray-950/65 to-gray-900/65 p-5 text-slate-300 backdrop-blur-2xl ${className}`}
       {...props}
     >
       {children}
