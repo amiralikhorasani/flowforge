912b653c42f24b95bfa6fdc59979772cb3984dd4
amiralikhorasani <amiralik163@gmail.com>
2026-08-10 22:17:30 +0330

    fix: resolve header and navbar issues

--- src/components/ui/header/Header.jsx
+++ src/components/ui/header/Header.jsx
@@ -38,12 +38,10 @@ function Header() {
         initial={{
           opacity: 0,
           y: -30,
-          filter: "blur(5px)",
         }}
         animate={{
           opacity: 1,
           y: 0,
-          filter: "blur(0px)",
         }}
         transition={{
           duration: 0.5,
--- src/components/ui/header/Navbar.jsx
+++ src/components/ui/header/Navbar.jsx
@@ -6,7 +6,7 @@ import NavbarItems from "./NavbarItems";
 
 function Navbar({ items, handlerOpenMenu }) {
   return (
-    <Container className="flex items-center justify-between px-4! py-6!">
+    <Container className="z-50! flex items-center justify-between px-4! py-6! shadow-md shadow-gray-900">
       <Logo />
 
       <div className="hidden lg:flex">
@@ -15,23 +15,23 @@ function Navbar({ items, handlerOpenMenu }) {
 
       <div className="flex items-center justify-center gap-2">
         <div className="hidden items-center justify-center gap-2 sm:flex">
-          <Button outline={true} to="/login">
+          <Button outline={true} to="/user/login">
             <LuLogIn size={20} />
             <span>Login</span>
           </Button>
 
-          <Button outline={true} to="/register">
+          <Button outline={true} to="/user/register">
             <LuUser size={20} />
             <span>Register</span>
           </Button>
         </div>
 
         <div className="flex items-center justify-center gap-2 sm:hidden">
-          <Button iconBtn={true} outline={true} to="/login">
+          <Button iconBtn={true} outline={true} to="/user/login">
             <LuLogIn size={20} />
           </Button>
 
-          <Button iconBtn={true} outline={true} to="/register">
+          <Button iconBtn={true} outline={true} to="/user/register">
             <LuUser size={20} />
           </Button>
         </div>
